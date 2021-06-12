7547bd07c68a1b8eb174aa49fc8808df4b20e438
Patricio Urrutia <purrutia@gmail.com>
2021-06-12 16:19:29 -0400

    Fires Commit

---
+++ rptree/rptree.py
@@ -0,0 +1,35 @@
+# rptree/rptree.py
+
+""" This module provides RP Tree main module. """
+
+import os
+import pathlib
+
+PIPE = "|"
+ELBOW = "└──"
+TEE = "├──"
+PIPE_PREFIX = "|    "
+SPACE_PREFIX = "     "
+
+class DirectoryTree:
+    def __init__(self, root_dir):
+        self._generator = _TreeGenerator(root_dir)
+
+    def generate(self):
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
+
+class _TreeGenerator:
+    def __init__(self, root_dir):
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = []
+
+    def build_tree(self):
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
